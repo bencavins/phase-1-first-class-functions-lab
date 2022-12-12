6b9e7749922a1c29ff7500cd032ac6dbb486f665
Ben Cavins <ben.cavins@flatironschool.com>
2022-12-12 14:43:12 -0500

    commit tree 5600621af9f0c43fbfd5e6976c04e602bd15f2f8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (list) => list.slice(0, 2);
+
+const returnLastTwoDrivers = (list) => list.slice(list.length-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return (fare) => fare * x;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers);
+}
